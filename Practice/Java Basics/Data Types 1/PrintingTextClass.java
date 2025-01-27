cbe052287c34a2ef507025f85e69f3ebefb5e00e
Christian Quintero <133067862+CJQuintero61@users.noreply.github.com>
2025-01-27 14:21:54 -0600

    Add files via upload

---
+++ Practice/Java Basics/Data Types 1/PrintingTextClass.java
@@ -0,0 +1,26 @@
+package printingtextpackage;
+
+public class PrintingTextClass 
+{
+
+	public static void main(String[] args) 
+	{
+		// this program is for practicing different Java 
+		// data types and printing them
+		
+		// make some random variables of different data types
+		String itemName = "Candy Bar";
+		int numberOfItems = 3;
+		float costOfItem = 2.99f;
+		float totalCost = costOfItem * numberOfItems;
+		char currency = '$';
+		
+		// print the variables 
+		System.out.println("Item: " + itemName);
+		System.out.println("Number of Items: " + numberOfItems);
+		System.out.println("Cost per Item: " + costOfItem);
+		System.out.println("Total cost: " + currency + totalCost);
+	
+	}
+
+}
